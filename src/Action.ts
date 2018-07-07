4e9710ba498226fb0645b53a5583f3a93e0426b5
Heinrich Muralt <heinrich@supertext.ch>
2018-07-07 14:14:56 +0200

    Removed StateDefinition (cumbersome  code and usage)

--- src/Action.ts
+++
@@ -1,9 +0,0 @@
-import { Action as ReduxAction } from "redux";
-
-export default abstract class Action implements ReduxAction {
-    constructor(public type: string) {
-    }
-}
-
-// tslint:disable-next-line:no-any
-export type ActionConstructorType<TAction, TActionType> = (new (...args: any[]) => TAction) & { type: TActionType };
--- src/DispatchingAction.ts
+++
@@ -1,5 +0,0 @@
-import { Dispatch } from "redux";
-
-export default abstract class DispatchingAction {
-    public abstract execute(dispatch: Dispatch): void;
-}
