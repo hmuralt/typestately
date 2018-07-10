285a75d77ea4ed41b72bdb645e83b1679f01d06e
Heinrich Muralt <heinrich@supertext.ch>
2018-07-10 11:21:46 +0200

    Extended StateHandlerUtils with readonly state handler

--- src/StateHandlerUtils.ts
+++ src/StateHandlerUtils.ts
@@ -1,6 +1,16 @@
 import StateHandler from "./StateHandler";
 import coreRegistry from "./CoreRegistry";
 
+class ReadonlyStateHandler<TState> extends StateHandler<TState> {
+    constructor(key: string, defaultState: TState) {
+        super(key, defaultState);
+    }
+}
+
+export function createReadonlyStateHandler<TState>(key: string, defaultState: TState) {
+    return new ReadonlyStateHandler(key, defaultState);
+}
+
 export function registerOnStore<TState>(storeId: string, stateHandler: StateHandler<TState>) {
     coreRegistry.registerState(storeId, stateHandler.stateReducer, stateHandler.statePublisher);
     stateHandler.onDispatch(coreRegistry.getStore(storeId).dispatch);
