cbc088e6bcaf62ac42ef89ff1ec49b48006540e7
Heinrich Muralt <heinrich@supertext.ch>
2018-07-14 14:32:15 +0200

    Better type annotation for WithStateToProps

--- src/components/WithStateToProps/WithStateToProps.tsx
+++ src/components/WithStateToProps/WithStateToProps.tsx
@@ -1,17 +1,10 @@
 import * as React from "react";
 import StateProvider, { Unsubscribe } from "../../StateProvider";
 
-// For the declaration file
-interface StateToPropsStatic {
-    componentDidMount(): void;
-    render(): void;
-    componentWillUnmount(): void;
-}
-
 export default function withStateToProps<TState, TProps>(
     stateProvider: StateProvider<TState>,
     stateToProps: (state: TState) => TProps
-): (Component: React.ComponentType<TProps>) => (new (props: {}) => StateToPropsStatic) {
+): (Component: React.ComponentType<TProps>) => React.ComponentClass {
     return (Component: React.ComponentType<TProps>) => {
         return class StateToProps extends React.PureComponent<{}, TProps> {
             private unsubscribe: Unsubscribe;
