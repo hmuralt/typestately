f27c39aaca0966863f73ca7903d7f9c56c0b3923
Heinrich Muralt <heinrich@supertext.ch>
2018-07-17 13:40:50 +0200

    Enhanced exports

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,5 @@ export * from "./StateHandlerUtils";
 export { default as StateProvider, Listener as StateListener, Unsubscribe as StateUnsubscribe } from "./StateProvider";
 export { default as StatePublisher } from "./StatePublisher";
 export { default as StateReducer } from "./StateReducer";
-export { default as WithStateToProps } from "./components/WithStateToProps/WithStateToProps";
-export { default as WithStore } from "./components/WithStore/WithStore";
+export { default as withStateToProps } from "./components/WithStateToProps/WithStateToProps";
+export { default as withStore } from "./components/WithStore/WithStore";
