07ec0bfc5638b31beecc929b0ac7f7e06919fa48
Heinrich Muralt <heinrich@supertext.ch>
2019-03-22 08:53:37 +0100

    Added ReducerHelpers

---
+++ src/ReducerHelpers.ts
@@ -0,0 +1,68 @@
+import { Reducer, Action } from "redux";
+import RouteAction, { isRouteAction } from "./RouteAction";
+import RoutingOption from "./RoutingOption";
+import DefaultStateReducer from "./DefaultStateReducer";
+
+export function withDefaultStateReducer<TState, TActionType>(
+  defaultState: TState,
+  reducer: Reducer<TState, Action<TActionType>>
+): DefaultStateReducer<TState, Action<TActionType>> {
+  return (state: TState | undefined = defaultState, action: Action<TActionType>) => {
+    return reducer(state, action);
+  };
+}
+
+export function withRouteReducer<TState, TActionType>(
+  identifier: string,
+  reducer: Reducer<TState, Action<TActionType>>,
+  routingOptions: Map<TActionType, RoutingOption>
+) {
+  return (state: TState, action: Action<TActionType> | RouteAction<TActionType>) => {
+    if (!isRouteAction(action)) {
+      return routingOptions.has(action.type) && routingOptions.get(action.type)!.isRoutedOnly
+        ? state
+        : reducer(state, action);
+    }
+
+    const actionToHandle = action.action;
+    const routingOption = routingOptions.get(actionToHandle.type) || {
+      isForThisInstance: true
+    };
+
+    if (
+      (!routingOption.isForThisInstance && !routingOption.isForOtherInstances) ||
+      (routingOption.isForThisInstance && !routingOption.isForOtherInstances && action.identifier !== identifier) ||
+      (routingOption.isForOtherInstances && !routingOption.isForThisInstance && action.identifier === identifier)
+    ) {
+      return state;
+    }
+
+    return reducer(state, actionToHandle);
+  };
+}
+
+export function createReducer<TState, TActionType>() {
+  const reducerFunctions = new Map<TActionType, Reducer<Readonly<TState>, Action<TActionType>>>();
+
+  function handling<TAction extends Action<TActionType>>(
+    type: TActionType,
+    reducerFunction: DefaultStateReducer<Readonly<TState>, TAction>
+  ) {
+    reducerFunctions.set(type, reducerFunction);
+    return reducer;
+  }
+
+  function reducer(state: TState, action: Action<TActionType>) {
+    if (!reducerFunctions.has(action.type)) {
+      return state;
+    }
+
+    const reducerFunction = reducerFunctions.get(action.type)!;
+
+    return reducerFunction(state, action);
+  }
+
+  reducer.handling = handling;
+
+  return reducer;
+}
---
+++ tests/ReducerHelpers.test.ts
@@ -0,0 +1,286 @@
+import { withDefaultStateReducer, withRouteReducer, createReducer } from "../src/ReducerHelpers";
+import { withRoute } from "../src/RouteAction";
+
+describe("withDefaultStateReducer", () => {
+  it("passes default state to reducer when undefined passed as state", () => {
+    // Arrange
+    const testDefaultState = {};
+    const reducer = jest.fn();
+    const testAction = { type: "testAction" };
+    const enhancedReducer = withDefaultStateReducer(testDefaultState, reducer);
+
+    // Act
+    enhancedReducer(undefined, testAction);
+
+    // Assert
+    expect(reducer.mock.calls[0][0]).toBe(testDefaultState);
+  });
+
+  it("returns state that was returned by reducer", () => {
+    // Arrange
+    const testDefaultState = {};
+    const reducer = jest.fn();
+    const returnedState = {};
+    reducer.mockReturnValue(returnedState);
+    const testAction = { type: "testAction" };
+    const enhancedReducer = withDefaultStateReducer(testDefaultState, reducer);
+
+    // Act
+    const result = enhancedReducer({}, testAction);
+
+    // Assert
+    expect(result).toBe(returnedState);
+  });
+});
+
+describe("withRouteReducer", () => {
+  const testIdentifier = "testIdentifier";
+  const testOtherIdentifier = "testOtherIdentifier";
+  const returnedState = {};
+  let testReducer: jest.Mock;
+
+  beforeEach(() => {
+    testReducer = jest.fn();
+    testReducer.mockReturnValue(returnedState);
+  });
+
+  describe("when passed action is not route action", () => {
+    it("calls reducer with state and action", () => {
+      // Arrange
+      const testState = {};
+      const testAction = { type: "testAction" };
+      const enhancedReducer = withRouteReducer(testIdentifier, testReducer, new Map());
+
+      // Act
+      enhancedReducer(testState, testAction);
+
+      // Assert
+      expect(testReducer).toHaveBeenCalledWith(testState, testAction);
+    });
+
+    it("returns state that was returned by reducer", () => {
+      // Arrange
+      const testState = {};
+      const testAction = { type: "testAction" };
+      const enhancedReducer = withRouteReducer(testIdentifier, testReducer, new Map());
+
+      // Act
+      const result = enhancedReducer(testState, testAction);
+
+      // Assert
+      expect(result).toBe(returnedState);
+    });
+
+    describe("when routed only action for this type of action", () => {
+      it("does not call reducer", () => {
+        // Arrange
+        const testState = {};
+        const testAction = { type: "testAction" };
+        const enhancedReducer = withRouteReducer(
+          testIdentifier,
+          testReducer,
+          new Map([["testAction", { isRoutedOnly: true }]])
+        );
+
+        // Act
+        enhancedReducer(testState, testAction);
+
+        // Assert
+        expect(testReducer).not.toHaveBeenCalled();
+      });
+
+      it("returns passed state", () => {
+        // Arrange
+        const testState = {};
+        const testAction = { type: "testAction" };
+        const enhancedReducer = withRouteReducer(
+          testIdentifier,
+          testReducer,
+          new Map([["testAction", { isRoutedOnly: true }]])
+        );
+
+        // Act
+        const result = enhancedReducer(testState, testAction);
+
+        // Assert
+        expect(result).toBe(testState);
+      });
+    });
+  });
+
+  describe("when passed action is route action", () => {
+    describe("when identifier matches", () => {
+      it("calls reducer with state and action", () => {
+        // Arrange
+        const testState = {};
+        const testAction = { type: "testAction" };
+        const testRouteAction = withRoute(testIdentifier, testAction);
+        const enhancedReducer = withRouteReducer(testIdentifier, testReducer, new Map());
+
+        // Act
+        enhancedReducer(testState, testRouteAction);
+
+        // Assert
+        expect(testReducer).toHaveBeenCalledWith(testState, testAction);
+      });
+
+      it("returns state that was returned by reducer", () => {
+        // Arrange
+        const testState = {};
+        const testAction = { type: "testAction" };
+        const testRouteAction = withRoute(testIdentifier, testAction);
+        const enhancedReducer = withRouteReducer(testIdentifier, testReducer, new Map());
+
+        // Act
+        const result = enhancedReducer(testState, testRouteAction);
+
+        // Assert
+        expect(result).toBe(returnedState);
+      });
+
+      describe("when option for other instances is set", () => {
+        it("does not call reducer", () => {
+          // Arrange
+          const testState = {};
+          const testAction = { type: "testAction" };
+          const testRouteAction = withRoute(testIdentifier, testAction);
+          const enhancedReducer = withRouteReducer(
+            testIdentifier,
+            testReducer,
+            new Map([["testAction", { isForOtherInstances: true }]])
+          );
+
+          // Act
+          enhancedReducer(testState, testRouteAction);
+
+          // Assert
+          expect(testReducer).not.toHaveBeenCalled();
+        });
+
+        it("returns passed state", () => {
+          // Arrange
+          const testState = {};
+          const testAction = { type: "testAction" };
+          const testRouteAction = withRoute(testIdentifier, testAction);
+          const enhancedReducer = withRouteReducer(
+            testIdentifier,
+            testReducer,
+            new Map([["testAction", { isForOtherInstances: true }]])
+          );
+
+          // Act
+          const result = enhancedReducer(testState, testRouteAction);
+
+          // Assert
+          expect(result).toBe(testState);
+        });
+      });
+    });
+
+    describe("when identifier does not match", () => {
+      it("does not call reducer", () => {
+        // Arrange
+        const testState = {};
+        const testAction = { type: "testAction" };
+        const testRouteAction = withRoute(testOtherIdentifier, testAction);
+        const enhancedReducer = withRouteReducer(testIdentifier, testReducer, new Map());
+
+        // Act
+        enhancedReducer(testState, testRouteAction);
+
+        // Assert
+        expect(testReducer).not.toHaveBeenCalled();
+      });
+
+      it("returns passed state", () => {
+        // Arrange
+        const testState = {};
+        const testAction = { type: "testAction" };
+        const testRouteAction = withRoute(testOtherIdentifier, testAction);
+        const enhancedReducer = withRouteReducer(testIdentifier, testReducer, new Map());
+
+        // Act
+        const result = enhancedReducer(testState, testRouteAction);
+
+        // Assert
+        expect(result).toBe(testState);
+      });
+
+      describe("when option for other instances is set", () => {
+        it("calls reducer with state and action", () => {
+          // Arrange
+          const testState = {};
+          const testAction = { type: "testAction" };
+          const testRouteAction = withRoute(testOtherIdentifier, testAction);
+          const enhancedReducer = withRouteReducer(
+            testIdentifier,
+            testReducer,
+            new Map([["testAction", { isForOtherInstances: true }]])
+          );
+
+          // Act
+          enhancedReducer(testState, testRouteAction);
+
+          // Assert
+          expect(testReducer).toHaveBeenCalledWith(testState, testAction);
+        });
+
+        it("returns state that was returned by reducer", () => {
+          // Arrange
+          const testState = {};
+          const testAction = { type: "testAction" };
+          const testRouteAction = withRoute(testOtherIdentifier, testAction);
+          const enhancedReducer = withRouteReducer(
+            testIdentifier,
+            testReducer,
+            new Map([["testAction", { isForOtherInstances: true }]])
+          );
+
+          // Act
+          const result = enhancedReducer(testState, testRouteAction);
+
+          // Assert
+          expect(result).toBe(returnedState);
+        });
+      });
+    });
+  });
+});
+
+describe("combineToReducer created reducer", () => {
+  const returnedState = {};
+  let testReducer: jest.Mock;
+
+  beforeEach(() => {
+    testReducer = jest.fn();
+    testReducer.mockReturnValue(returnedState);
+  });
+
+  it("returns state that was returned by reducer", () => {
+    // Arrange
+    const testState = {};
+    const testAction = { type: "testAction" };
+    const reducer = createReducer<{}, string>().handling("testAction", testReducer);
+
+    // Act
+    const result = reducer(testState, testAction);
+
+    // Assert
+    expect(result).toBe(returnedState);
+  });
+
+  describe("when action type not handled by any function", () => {
+    it("returns passed state", () => {
+      // Arrange
+      const testState = {};
+      const testAction = { type: "testAnotherAction" };
+      const reducer = createReducer<{}, string>().handling("testAction", testReducer);
+
+      // Act
+      const result = reducer(testState, testAction);
+
+      // Assert
+      expect(result).toBe(testState);
+    });
+  });
+});
