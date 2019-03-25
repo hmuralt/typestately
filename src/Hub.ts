ca8508c2511daf4811707b3821a863b093123cec
Heinrich Muralt <heinrich@supertext.ch>
2019-03-25 15:18:42 +0100

    Make hub destructible

--- src/Hub.ts
+++ src/Hub.ts
@@ -1,8 +1,9 @@
 import { Action, Reducer } from "redux";
-import { createDirigiblePublisher } from "./DirigiblePublisher";
+import DirigiblePublisher, { createDirigiblePublisher } from "./DirigiblePublisher";
+import { DestructibleResource } from "./Destructible";
 
-export interface ActionNotification {
-  parentcontextId: string;
+export interface DispatchedActionNotification {
+  parentContextId: string;
   action: Action;
 }
 
@@ -11,7 +12,7 @@ export interface DestructionNotification {
 }
 
 export interface ReducerNotification {
-  parentcontextId: string;
+  parentContextId: string;
   key: string;
 }
 
@@ -24,8 +25,37 @@ export interface StateNotification {
   state: {};
 }
 
-export const dispatchedActionPublisher = createDirigiblePublisher<ActionNotification>();
-export const destructionPublisher = createDirigiblePublisher<DestructionNotification>();
-export const reducerRegistrationPublisher = createDirigiblePublisher<ReducerRegistrationNotification>();
-export const reducerDeregistrationPublisher = createDirigiblePublisher<ReducerNotification>();
-export const statePublisher = createDirigiblePublisher<StateNotification>();
+export interface Hub {
+  dispatchedActionPublisher: DirigiblePublisher<DispatchedActionNotification>;
+  destructionPublisher: DirigiblePublisher<DestructionNotification>;
+  reducerRegistrationPublisher: DirigiblePublisher<ReducerRegistrationNotification>;
+  reducerDeregistrationPublisher: DirigiblePublisher<ReducerNotification>;
+  statePublisher: DirigiblePublisher<StateNotification>;
+}
+
+export function createHub(): DestructibleResource<Hub> {
+  const dispatchedActionPublisher = createDirigiblePublisher<DispatchedActionNotification>();
+  const destructionPublisher = createDirigiblePublisher<DestructionNotification>();
+  const reducerRegistrationPublisher = createDirigiblePublisher<ReducerRegistrationNotification>();
+  const reducerDeregistrationPublisher = createDirigiblePublisher<ReducerNotification>();
+  const statePublisher = createDirigiblePublisher<StateNotification>();
+
+  const hub = {
+    dispatchedActionPublisher: dispatchedActionPublisher.object,
+    destructionPublisher: destructionPublisher.object,
+    reducerRegistrationPublisher: reducerRegistrationPublisher.object,
+    reducerDeregistrationPublisher: reducerDeregistrationPublisher.object,
+    statePublisher: statePublisher.object
+  };
+
+  return {
+    object: hub,
+    destroy() {
+      dispatchedActionPublisher.destroy();
+      destructionPublisher.destroy();
+      reducerRegistrationPublisher.destroy();
+      reducerDeregistrationPublisher.destroy();
+      statePublisher.destroy();
+    }
+  };
+}
---
+++ tests/Hub.test.ts
@@ -0,0 +1,37 @@
+import { createHub } from "../src/Hub";
+import { createDirigiblePublisher } from "../src/DirigiblePublisher";
+
+jest.mock("../src/DirigiblePublisher");
+
+describe("Hub", () => {
+  describe("when destroyed", () => {
+    let destroyFunctions: Array<() => void>;
+
+    beforeEach(() => {
+      destroyFunctions = [];
+
+      (createDirigiblePublisher as jest.Mock).mockImplementation(() => {
+        const destroy = jest.fn();
+        destroyFunctions.push(destroy);
+        return {
+          destroy
+        };
+      });
+    });
+
+    it("destroys all publishers", () => {
+      // Arrange
+      const { destroy } = createHub();
+
+      // Act
+      destroy();
+
+      // Assert
+      expect(destroyFunctions[0]).toHaveBeenCalled();
+      expect(destroyFunctions[1]).toHaveBeenCalled();
+      expect(destroyFunctions[2]).toHaveBeenCalled();
+      expect(destroyFunctions[3]).toHaveBeenCalled();
+      expect(destroyFunctions[4]).toHaveBeenCalled();
+    });
+  });
+});
