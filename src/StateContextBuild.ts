ef9df8cbd8749f86df2df21a111745d02752b89a
Heinrich Muralt <heinrich@supertext.ch>
2019-05-12 14:12:59 +0200

    Refactored StateContextBuild

--- src/StateContextBuild.ts
+++ src/StateContextBuild.ts
@@ -5,23 +5,23 @@ import { Hub } from "./Hub";
 import { storeContextId } from "./StoreContext";
 import DefaultStateReducer from "./DefaultStateReducer";
 
-export interface StateContextDefinition<TState, TActionType> {
+export interface AttachableStateDefinition<TState, TActionType> {
   attachTo(hub: Hub, parentContextId?: string): StateContext<TState, TActionType>;
 }
 
-export interface StateContextStructure<TState, TActionType> {
+export interface StateDefinition<TState, TActionType> {
   setReducer(
     reducer?: DefaultStateReducer<Readonly<TState>, Action<TActionType>>,
     routingOptions?: Map<TActionType, RoutingOption>
-  ): StateContextDefinition<TState, TActionType>;
+  ): AttachableStateDefinition<TState, TActionType>;
 }
 
 // tslint:disable-next-line: no-any
-export function buildStateContext<TState = {}, TActionType = any>(
+export function createStateDefinition<TState = {}, TActionType = any>(
   key: string,
   defaultState: TState,
   stateKey = "state"
-): StateContextStructure<TState, TActionType> {
+): StateDefinition<TState, TActionType> {
   return {
     setReducer(
       reducer?: DefaultStateReducer<Readonly<TState>, Action<TActionType>>,
