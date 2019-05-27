f1c00534ecd459a5a5da2d5423c4742ed6dd67db
Heinrich Muralt <heinrich@supertext.ch>
2019-05-27 11:45:49 +0200

    Updated barrel exports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export * from "./StateContext";
 export * from "./StoreContext";
 export * from "./StateContextBuild";
+export { Hub } from "./Hub";
 export { createExtensibleReducer } from "./ReducerHelpers";
-export { default as withStateToProps } from "./components/WithStateToProps/WithStateToProps";
+export { default as withStateToProps, StateProvider } from "./components/WithStateToProps/WithStateToProps";
 export { default as StateHandler } from "./StateHandler";
